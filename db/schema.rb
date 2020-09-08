32172005582c7694059995948bdb9af2e220d089
Addison Holbrook <holbrooa@gmail.com>
2020-09-08 18:49:42 +0200

    new project form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_230812) do
+ActiveRecord::Schema.define(version: 2020_09_08_093815) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_230812) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.string "city"
+    t.string "pay_type"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
