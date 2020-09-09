d247f02ab1fa0164f2df8a110311a9f97f27b43c
Roman <frontandstart@gmail.com>
2020-09-09 11:54:09 +0300

    fix: email SMTP config for production
    Added mailcatcher for developmept

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :cloudinary
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
   config.active_storage.service = :cloudinary
@@ -63,4 +63,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.default_url_options = { host: "http://localhost:3000" }
 end
